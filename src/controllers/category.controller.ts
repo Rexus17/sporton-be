a0f61339895eccec3f5ace811fa1517e6600a96a
Ahmad Rifai <94911386+Rexus17@users.noreply.github.com>
2026-02-16 15:39:45 +0700

    Update category.controller.ts

--- src/controllers/category.controller.ts
+++ src/controllers/category.controller.ts
@@ -1,6 +1,10 @@
 import {Request, Response} from "express";
 import Category from "../models/category.model";
 
+interface MulterRequest extends Request {
+  file?: Express.Multer.File;
+}
+
 export const createCategory = async (req: Request, res: Response): Promise<void> => {
     try {
         const categoryData = req.body;
@@ -77,3 +81,4 @@ export const deleteCategory = async (req: Request, res: Response): Promise<void>
         res.status(500).json({message: "Error deleting category", error});
     }
 }
+
